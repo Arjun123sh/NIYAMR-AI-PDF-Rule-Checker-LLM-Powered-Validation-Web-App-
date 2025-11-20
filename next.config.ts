0c8a91e47e73217f8c8c9a3ecc1329c1c36368f7
Arjun Sharma <arjunrocks405@gmail.com>
2025-11-20 22:56:51 +0530

    initials commit

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,16 @@ const nextConfig: NextConfig & Record<string, unknown> = {
       experimental: {
         serverComponentsExternalPackages: ['pdf2json'],
     },
-    
+     tsconfig: {
+    ignoreBuildErrors: true,
+  },
+
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+eslint: {
+  ignoreDuringBuilds: true,
+},
     
     swcMinify: true,
     compress: true,
